904132f76f00d630cee6bef540073f7dde845273
Andriy Kulak <akulak@fuzzproductions.com>
2018-06-26 22:55:31 -0400

    ak added basic contract structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router'
-import { HiddenOnlyAuth, VisibleOnlyAuth } from './util/wrappers.js'
-import getWeb3 from '.utils/getWeb3'
+import SimpleStorageContract from '../build/contracts/SimpleStorage.json'
+import { HiddenOnlyAuth, VisibleOnlyAuth } from './util/wrappers'
+import getWeb3 from './util/getWeb3'
 
 // UI Components
 import LoginButtonContainer from './user/ui/loginbutton/LoginButtonContainer'
@@ -14,6 +15,82 @@ import './css/pure-min.css'
 import './App.css'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      storageValue: 0,
+      web3: null,
+      contract: null,
+      account: null
+    }
+  }
+
+  componentWillMount() {
+    // Get network provider and web3 instance.
+    // See utils/getWeb3 for more info.
+
+    getWeb3
+    .then(results => {
+      this.setState({
+        web3: results.web3
+      })
+
+      // Instantiate contract once web3 provided.
+      this.instantiateContract()
+    })
+    .catch(() => {
+      console.log('Error finding web3.')
+    })
+  }
+
+  instantiateContract() {
+    /*
+     * SMART CONTRACT EXAMPLE
+     *
+     * Normally these functions would be called in the context of a
+     * state management library, but for convenience I've placed them here.
+     */
+
+    const contract = require('truffle-contract')
+    const simpleStorage = contract(SimpleStorageContract)
+    simpleStorage.setProvider(this.state.web3.currentProvider)
+
+    // Declaring this for later so we can chain functions on SimpleStorage.
+    var simpleStorageInstance
+
+    // Get accounts.
+    this.state.web3.eth.getAccounts((error, accounts) => {
+      simpleStorage.deployed().then((instance) => {
+        simpleStorageInstance = instance
+
+        // Stores a given value, 5 by default.
+        return simpleStorageInstance.set(5, {from: accounts[0]})
+      }).then((result) => {
+        // Get the value from the contract to prove it worked.
+        return simpleStorageInstance.get.call(accounts[0])
+      }).then((result) => {
+        // Update state with the result.
+        // AK_ADDED
+        return this.setState({ storageValue: result.c[0], contract: simpleStorageInstance, account: accounts[0] })
+      })
+    })
+  }
+
+  handleClick(event) {
+    const contract = this.state.contract
+    const account = this.state.account
+
+    const value = 3
+
+    contract.set(value, {from: account})
+      .then(result => {
+        return contract.get.call()
+      }).then(result => {
+        return this.setState({ storageValue: result.c[0]})
+      })
+  }
+
   render() {
     const OnlyAuthLinks = VisibleOnlyAuth(() =>
       <span>
@@ -36,13 +113,15 @@ class App extends Component {
     return (
       <div className="App">
         <nav className="navbar pure-menu pure-menu-horizontal">
+          <div style={{color:'white'}}>
+            <p>The stored value is: {this.state.storageValue}</p>
+          </div>
           <Link to="/" className="pure-menu-heading pure-menu-link">Truffle Box Test</Link>
           <ul className="pure-menu-list navbar-right">
             <OnlyGuestLinks />
             <OnlyAuthLinks />
           </ul>
         </nav>
-
         {this.props.children}
       </div>
     );
